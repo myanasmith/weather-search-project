a217a66738da0a9a34ab6499b30980fe9c84af03
Myana <myanasmith2@gmail.com>
2024-11-03 11:51:20 -0800

    Added a new function to replace the weather temp from search

--- src/app.js
+++ src/app.js
@@ -1,8 +1,21 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+  console.log(response.data.temperature.current);
+}
+
+function searchCity(city) {
+  let apiKey = "e39fdf3becea3o599dbd0et40dac045a";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#weather-app-city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormEement = document.querySelector("#search-form");
