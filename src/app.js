ce7b2fac10f90a94bf2718c1771ad11d81744f0f
Myana <myanasmith2@gmail.com>
2024-11-03 14:10:08 -0800

    Added iconElment w/ img selector

--- src/app.js
+++ src/app.js
@@ -7,6 +7,9 @@ function updateWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date();
+  let iconElement = document.querySelector("#icon");
+
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon"/>`;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
